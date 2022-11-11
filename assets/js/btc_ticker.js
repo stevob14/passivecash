6033ea62cb139515ba851fb57d49cd4f0c63ea62
stevo <stevob144@gmail.com>
2022-11-11 05:22:23 -0600

    Create btc_ticker.js

---
+++ assets/js/btc_ticker.js
@@ -0,0 +1,23 @@
+function get_price() {
+      fetch("https://api.pro.coinbase.com/products/btc-usd/book")
+        .then(res => res.json())
+        .then(res => {
+          var price = numberWithCommas(res.bids[0][0]);
+          $(".Price").html("$"+price)     
+          changePageTitle(price)
+        }).catch(err => {
+          $(".Price").html("Error")
+        });
+    }
+
+    get_price()
+
+    setInterval(get_price, 1000)
+ 
+ function numberWithCommas(res){
+ return res.toString().replace(/\B(?=(\d{3})+(?!\d))/g,",")
+ }
+    
+ function changePageTitle(price) {
+  document.querySelector('title').textContent= price + " | BTCUSD";
+        }
