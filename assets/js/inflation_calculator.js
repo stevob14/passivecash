5f397f721f10d062166b3b3de473ae9a6037b245
stevo <stevob144@gmail.com>
2022-11-15 06:44:35 -0600

    Create inflation_calculator.js

---
+++ assets/js/inflation_calculator.js
@@ -0,0 +1,147 @@
+cpi = new Object(); 
+cpi.OLDEST_YEAR = 1913; 
+cpi.LATEST_YEAR = 2021; 
+
+cpi.rates = new Array(3); 
+cpi.rates[0] = 9.9;   //1913 
+cpi.rates[1] = 10;    //1914
+cpi.rates[2] = 10.1; 	//1915
+cpi.rates[3] = 10.9; 	//1916
+cpi.rates[4] = 12.8; 	//1917
+cpi.rates[5] = 15.1; 	//1918
+cpi.rates[6] = 17.3; 	//1919
+cpi.rates[7] = 20.0; 	//1920
+cpi.rates[8] = 17.9;	//1921
+cpi.rates[9] = 16.8;	//1922
+cpi.rates[10] = 17.1;	//1923
+cpi.rates[11] = 17.1;	//1924
+cpi.rates[12] = 17.5;	//1925
+cpi.rates[13] = 17.7;	//1926
+cpi.rates[14] = 17.4;	//1927
+cpi.rates[15] = 17.1;	//1928
+cpi.rates[16] = 17.1;	//1929
+cpi.rates[17] = 16.7;	//1930
+cpi.rates[18] = 15.2;	//1931
+cpi.rates[19] = 13.7;	//1932
+cpi.rates[20] = 13.0;	//1933
+cpi.rates[21] = 13.4;	//1934
+cpi.rates[22] = 13.7;	//1935
+cpi.rates[23] = 13.9;	//1936
+cpi.rates[24] = 14.4;	//1937
+cpi.rates[25] = 14.1;	//1938
+cpi.rates[26] = 13.9;	//1939
+cpi.rates[27] = 14.0;	//1940
+cpi.rates[28] = 14.7;	//1941
+cpi.rates[29] = 16.3;	//1942
+cpi.rates[30] = 17.3;	//1943
+cpi.rates[31] = 17.6;	//1944
+cpi.rates[32] = 18.0;	//1945
+cpi.rates[33] = 19.5;	//1946
+cpi.rates[34] = 22.3;	//1947
+cpi.rates[35] = 24.1;	//1948
+cpi.rates[36] = 23.8;	//1949
+cpi.rates[37] = 24.1;	//1950
+cpi.rates[38] = 26.0;	//1951
+cpi.rates[39] = 26.5;	//1952
+cpi.rates[40] = 26.7;	//1953
+cpi.rates[41] = 26.9;	//1954
+cpi.rates[42] = 26.8;	//1955
+cpi.rates[43] = 27.2;	//1956
+cpi.rates[44] = 28.1;	//1957
+cpi.rates[45] = 28.9;	//1958
+cpi.rates[46] = 29.1;	//1959
+cpi.rates[47] = 29.6;	//1960
+cpi.rates[48] = 29.9;	//1961
+cpi.rates[49] = 30.2;	//1962
+cpi.rates[50] = 30.6;	//1963
+cpi.rates[51] = 31.0;	//1964
+cpi.rates[52] = 31.5;	//1965
+cpi.rates[53] = 32.4;	//1966
+cpi.rates[54] = 33.4;	//1967
+cpi.rates[55] = 34.8;	//1968
+cpi.rates[56] = 36.7;	//1969
+cpi.rates[57] = 38.8;	//1970
+cpi.rates[58] = 40.5;	//1971
+cpi.rates[59] = 41.8;	//1972
+cpi.rates[60] = 44.4;	//1973
+cpi.rates[61] = 49.3;	//1974
+cpi.rates[62] = 53.8;	//1975
+cpi.rates[63] = 56.9;	//1976
+cpi.rates[64] = 60.6;	//1977
+cpi.rates[65] = 65.2;	//1978
+cpi.rates[66] = 72.6;	//1979
+cpi.rates[67] = 82.4;	//1980
+cpi.rates[68] = 90.9;	//1981
+cpi.rates[69] = 96.5;	//1982
+cpi.rates[70] = 99.6;	//1983
+cpi.rates[71] = 103.9;	//1984
+cpi.rates[72] = 107.6;	//1985
+cpi.rates[73] = 109.6;	//1986
+cpi.rates[74] = 113.6;	//1987
+cpi.rates[75] = 118.3;	//1988
+cpi.rates[76] = 124.0;	//1989
+cpi.rates[77] = 130.7;	//1990
+cpi.rates[78] = 136.2;	//1991
+cpi.rates[79] = 140.3;	//1992
+cpi.rates[80] = 144.5;	//1993
+cpi.rates[81] = 148.2;	//1994
+cpi.rates[82] = 152.4;	//1995
+cpi.rates[83] = 156.9;	//1996
+cpi.rates[84] = 160.5;	//1997
+cpi.rates[85] = 163.0;	//1998
+cpi.rates[86] = 166.6;	//1999
+cpi.rates[87] = 172.2;	//2000
+cpi.rates[88] = 177.1;	//2001
+cpi.rates[89] = 179.9;	//2002
+cpi.rates[90] = 184.0;	//2003
+cpi.rates[91] = 188.9;	//2004
+cpi.rates[92] = 195.3;	//2005
+cpi.rates[93] = 201.6;	//2006
+cpi.rates[94] = 207.3;	//2007
+cpi.rates[95] = 215.303;	//2008
+cpi.rates[96] = 214.537;	//2009
+cpi.rates[97] = 218.056;	//2010
+cpi.rates[98] = 224.939;	//2011
+cpi.rates[99] = 229.594;	//2012
+cpi.rates[100] = 232.957;	//2013
+cpi.rates[101] = 236.736;	//2014
+cpi.rates[102] = 237.017;	//2015
+cpi.rates[103] = 240.007;	//2016
+cpi.rates[104] = 245.120;	//2017
+cpi.rates[105] = 251.107;	//2018
+cpi.rates[106] = 255.657;	//2019
+cpi.rates[107] = 258.811;	//2020
+cpi.rates[108] = 270.970;	//2021
+
+// data provided by https://www.usinflationcalculator.com/inflation/consumer-price-index-and-annual-percent-changes-from-1913-to-2008/
+
+function calculate() {	// Calculate button 
+	//1970 Price x (2011 CPI / 1970 CPI) = 2011 Price
+  dollar = Number(document.getElementById("dollarField").value);
+  start = Number(document.getElementById("startYearField").value);
+  end = Number(document.getElementById("endYearField").value); 
+  
+  if (start >= 1913 && start <= 2021 && end >=1913 && end <=2021)
+  {
+  start_array = start - cpi.OLDEST_YEAR
+  end_array = end - cpi.OLDEST_YEAR
+  start_cpi = cpi.rates[start_array]
+  end_cpi = cpi.rates[end_array]
+  result = dollar * (end_cpi/start_cpi)
+  document.getElementById("output").innerHTML = "$" + dollar + " in " + start + " is equivalent in purchasing power to ~" + result.toLocaleString("en-US",{style:'currency',currency: 'USD', maximumFractionDigits:2}) + " in " + end;
+  }
+  else{
+  error = "Data only available for years in range 1913-2021";
+  document.getElementById("output").innerHTML = error;
+  }
+} 
+
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode === 8 || charCode === 46)
+				return true;
+    else if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    else
+    		return true;
+}
