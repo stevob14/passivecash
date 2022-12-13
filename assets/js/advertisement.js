b5756a7a2e6f2fc02206a1ea81283e7549674579
stevo <stevob144@gmail.com>
2022-12-13 06:45:23 -0600

    Update advertisement.js

--- assets/js/advertisement.js
+++ assets/js/advertisement.js
@@ -2,7 +2,8 @@ const ads = []
 ads[0] = '<a href="https://join.robinhood.com/stephec1028" style="color: #ffa500 !important;">Robinhood - Get up to $200 in free stock</a>'
 ads[1] = '<a href="https://app.groundfloor.us/r/od0e9f" style="color: #ffa500 !important;">Groundfloor - Get $50 when you invest $100</a>'
 ads[2] = '<a href="https://coinbase.com/join/crooks_r" style="color: #ffa500 !important;">Coinbase - Get free Bitcoin worth $10</a>'
+ads[3] = '<a href="https://fundrise.com/r/4k7r3v" style="color: #ffa500 !important;">Fundrise - Get $25 in shares when you invest $10</a>'
 
-random = Math.floor(Math.random() * 3);
+random = Math.floor(Math.random() * 4);
 
 document.getElementById("advertisement").innerHTML =  ads[random]
