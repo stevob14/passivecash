459e4778136576d30c72ede08feed02c807b264b
stevo <stevob144@gmail.com>
2023-01-22 02:40:34 -0600

    Update news.js

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,20 +1,20 @@
 const RSS_URLS = [
-  "https://www.economist.com/finance-and-economics/rss.xml",
-  "https://www.cnbc.com/id/100727362/device/rss/rss.html",
-  "https://www.marketwatch.com/rss/marketpulse",
-  "https://www.forbes.com/business/feed/",
-  "https://www.ft.com/rss/home/us",
-  "https://www.businessinsider.com/rss",
-  "http://rss.cnn.com/rss/money_news_international.rss",
-  "https://www.newyorktimes.com/services/xml/rss/nyt/Business.xml",
-  "https://rssfeeds.usatoday.com/UsatodaycomBusiness-TopStories",
-  "https://www.investors.com/feed/",
-  "https://www.entrepreneur.com/rss/topic/business",
-  "https://www.venturebeat.com/category/business/feed/",
-  "https://techcrunch.com/startups/feed/",
-  "https://thenextweb.com/feed/", 
-  "https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml",
-  "https://www.finance.yahoo.com/news/rss/"
+  "https://cors-anywhere.herokuapp.com/https://www.economist.com/finance-and-economics/rss.xml",
+  "https://cors-anywhere.herokuapp.com/https://www.cnbc.com/id/100727362/device/rss/rss.html",
+  "https://cors-anywhere.herokuapp.com/https://www.marketwatch.com/rss/marketpulse",
+  "https://cors-anywhere.herokuapp.com/https://www.forbes.com/business/feed/",
+  "https://cors-anywhere.herokuapp.com/https://www.ft.com/rss/home/us",
+  "https://cors-anywhere.herokuapp.com/https://www.businessinsider.com/rss",
+  "https://cors-anywhere.herokuapp.com/http://rss.cnn.com/rss/money_news_international.rss",
+  "https://cors-anywhere.herokuapp.com/https://www.newyorktimes.com/services/xml/rss/nyt/Business.xml",
+  "https://cors-anywhere.herokuapp.com/https://rssfeeds.usatoday.com/UsatodaycomBusiness-TopStories",
+  "https://cors-anywhere.herokuapp.com/https://www.investors.com/feed/",
+  "https://cors-anywhere.herokuapp.com/https://www.entrepreneur.com/rss/topic/business",
+  "https://cors-anywhere.herokuapp.com/https://www.venturebeat.com/category/business/feed/",
+  "https://cors-anywhere.herokuapp.com/https://techcrunch.com/startups/feed/",
+  "https://cors-anywhere.herokuapp.com/https://thenextweb.com/feed/", 
+  "https://cors-anywhere.herokuapp.com/https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml",
+  "https://cors-anywhere.herokuapp.com/https://www.finance.yahoo.com/news/rss/"
 ];
 
 const fetchFeed = async (url) => {
