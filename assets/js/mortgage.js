7de477479008a5ef35d7ec3d16ea9cf9bf267c6b
stevo <stevob144@gmail.com>
2023-03-29 06:58:14 -0500

    Update mortgage.js

--- assets/js/mortgage.js
+++ assets/js/mortgage.js
@@ -1,10 +1,32 @@
 function calculateMortgage() {
         const loanAmount = parseFloat(document.getElementById("loanAmount").value);
         const downPayment = parseFloat(document.getElementById("downPayment").value);
-        const interestRate = parseFloat(document.getElementById("interestRate").value);
+        const interestRate = parseFloat(document.getElementById("interestRate").value) ;
         const loanTerm = parseFloat(document.getElementById("loanTerm").value);
-        const propertyTaxes = parseFloat(document.getElementById("propertyTaxes").value);
-        const homeInsurance = parseFloat(document.getElementById("homeInsurance").value);
+        const propertyTaxes = parseFloat(document.getElementById("propertyTaxes").value) || 0;
+        const homeInsurance = parseFloat(document.getElementById("homeInsurance").value) || 0;
+        
+let errorMsg = "";
+        if (loanAmount <= 0 || isNaN(loanAmount)) {
+          errorMsg += "<p>Please enter a positive loan amount.</p>";
+        }
+        if (downPayment < 0 || isNaN(downPayment)) {
+          errorMsg += "<p>Please enter a non-negative down payment.</p>";
+        }
+        if (interestRate <=0 || isNaN(interestRate)) {
+          errorMsg += "<p>Please enter a positive interest rate.</p>";
+        }
+        if (loanTerm <= 0 || isNaN(loanTerm)) {
+          errorMsg += "<p>Please enter a positive loan term.</p>";
+        }
+        
+        if (errorMsg !== "") {
+          document.getElementById("errorMessages").innerHTML = errorMsg;
+          document.getElementById("resultTable").style.display = "none";
+          return;
+        } else {
+          document.getElementById("errorMessages").innerHTML = "";
+        }
         
         const monthlyInterestRate = (interestRate / 100) / 12;
         const numberOfPayments = loanTerm * 12;
@@ -27,5 +49,9 @@ function calculateMortgage() {
            document.getElementById("totalInterest").innerHTML = "$" + totalInterest;
     document.getElementById("propertyTaxesMonthly").innerHTML = "$" + monthlyPropertyTaxes;
     document.getElementById("homeInsuranceMonthly").innerHTML = "$" + monthlyHomeInsurance;
+   
+    console.log(loanAmount)
     document.getElementById("resultTable").style.display = "block";
+    
+    
   }
