83229123e529cada9061f312baae1062f54d23cb
stevo <stevob144@gmail.com>
2023-06-05 00:00:09 -0500

    Update advertisement.js

--- assets/js/advertisement.js
+++ assets/js/advertisement.js
@@ -1,6 +1,6 @@
 const ads = []
 ads[0] = '<a href="https://join.robinhood.com/stephec1028" style="color: #ffa500 !important;">Robinhood - Get up to $200 in free stock</a>'
-ads[1] = '<a href="https://app.groundfloor.us/r/od0e9f" style="color: #ffa500 !important;">Groundfloor - Get $100 when you invest $1000</a>'
+ads[1] = '<a href="https://app.groundfloor.us/r/od0e9f" style="color: #ffa500 !important;">Groundfloor - Get $50 when you invest $100</a>'
 ads[2] = '<a href="https://coinbase.com/join/crooks_r" style="color: #ffa500 !important;">Coinbase - Get free Bitcoin worth $10</a>'
 ads[3] = '<a href="https://fundrise.com/r/4k7r3v" style="color: #ffa500 !important;">Fundrise - Get $50 in shares when you invest $10</a>'
 
