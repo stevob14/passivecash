4554dea70e8901baa76d500dbc1d62bcfe6f9595
stevo <stevob144@gmail.com>
2025-02-06 06:08:54 -0600

    Update rebalancer.js

--- assets/js/rebalancer.js
+++ assets/js/rebalancer.js
@@ -66,5 +66,4 @@ function removeInvestment(button) {
   const row = button.parentNode.parentNode;
   const table = document.getElementById("investmentTable");
   table.deleteRow(row.rowIndex - 1);
-  calculateRebalance();
 }
