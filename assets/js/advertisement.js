f5241a04de8bd37d74ccbc8d4d1edd73761d25fe
stevo <stevob144@gmail.com>
2025-04-04 05:03:28 -0500

    Update advertisement.js

--- assets/js/advertisement.js
+++ assets/js/advertisement.js
@@ -2,7 +2,8 @@ const ads = []
 ads[0] = '<a href="https://join.robinhood.com/stephec1028" style="color: #ffa500 !important;">Robinhood - Get up to $200 in free stock</a>'
 ads[1] = '<a href="https://coinbase.com/join/MG2TWZF" style="color: #ffa500 !important;">Coinbase - Get $20 after your first trade of $20 or more</a>'
 ads[2] = '<a href="https://a.webull.com/iHwte0AKHLHI3C70Xf" style="color: #ffa500 !important;">Webull - Get 12 FREE stocks</a>'
+ads[3] = '<a href="https://twitter.com/passivecashxyz" style="color: #ffa500 !important;">Follow PassiveCash.xyz on X</a>'
 
-random = Math.floor(Math.random() * 3);
+random = Math.floor(Math.random() * 4);
 
 document.getElementById("advertisement").innerHTML =  ads[random]
